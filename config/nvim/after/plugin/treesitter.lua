514f1db36e6bae414eda66120c040ebcc6fc55d7
Nicholas Anderson <nicholas.anderson@konghq.com>
2025-02-23 17:03:15 -0800

    feat(tree-sitter): enable folding

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,70 +1,76 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = {
-		-- system
-		"awk",
-		"bash",
-		"c",
-		"lua",
-		"luadoc",
-		"editorconfig",
-		"make",
-		"markdown",
-		"mermaid",
-		"perl",
-		"properties",
-		"ssh_config",
-		"toml",
-		"vim",
-		"vimdoc",
+    ensure_installed = {
+        -- system
+        "awk",
+        "bash",
+        "c",
+        "lua",
+        "luadoc",
+        "editorconfig",
+        "make",
+        "markdown",
+        "mermaid",
+        "perl",
+        "properties",
+        "ssh_config",
+        "toml",
+        "vim",
+        "vimdoc",
 
-		-- deployment
-		"arduino",
-		"dockerfile",
-		"hcl",
-		"helm",
-		"jq",
-		"json",
-		"starlark",
-		"sql",
-		"terraform",
-		"yaml",
+        -- deployment
+        "arduino",
+        "dockerfile",
+        "hcl",
+        "helm",
+        "jq",
+        "json",
+        "starlark",
+        "sql",
+        "terraform",
+        "yaml",
 
-		-- sadness
-		"groovy",
-		"java",
-		"kotlin",
-		"python",
-		"scala",
+        -- sadness
+        "groovy",
+        "java",
+        "kotlin",
+        "python",
+        "scala",
 
-		-- git
-		"git_config",
-		"git_rebase",
-		"gitattributes",
-		"gitcommit",
-		"gitignore",
+        -- git
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
 
-		-- training
-		"rust",
-		"zig",
+        -- training
+        "rust",
+        "zig",
 
-		-- golang
-		"go",
-		"goctl",
-		"gomod",
-		"gosum",
-		"gotmpl",
-		"gowork",
+        -- golang
+        "go",
+        "goctl",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "gowork",
 
-		-- tech bro
-		"javascript",
-		"typescript",
-		"vue",
-	},
+        -- tech bro
+        "javascript",
+        "typescript",
+        "vue",
+    },
 
-	auto_install = true,
+    auto_install = true,
 
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	}
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    }
 }
+
+local vim = vim
+local opt = vim.opt
+
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
