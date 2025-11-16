33ec8539fdfbd42b9f12e753fc75ec6a853cb891
Nick Anderson <nick@morethq.com>
2025-11-15 19:03:30 -0800

    feat(ui): add markdown preview

--- src/nvim/.config/nvim/lua/iamnande/editor/ui.lua
+++ src/nvim/.config/nvim/lua/iamnande/editor/ui.lua
@@ -18,4 +18,13 @@ return {
 			vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 		end,
 	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 }
