14232f5bc06c14d2e56e43dd69d1426e9ee2610b
Nick Anderson <nick@morethq.com>
2026-01-10 11:31:24 -0800

    fix(aarch64): resolve os specific issues
    * hyprland got an update, the lines removed are no longer meaningful
    * fnm is a "work" thing, sort of - i'll think about it another time
    * who doesn't love a neovim lazy update?
    
    brought to you by.. yourself

--- src/zsh/.config/zsh/10-prompt.zsh
+++ src/zsh/.config/zsh/10-prompt.zsh
@@ -1,6 +1,7 @@
 # fencing, fighting, torture
 eval "$(starship init zsh)"
-eval "$(fnm env --use-on-cd --shell zsh)"
+# TODO(nick): move this to macos, or use it, or remove it entirely
+#eval "$(fnm env --use-on-cd --shell zsh)"
 
 # we are but poor, lost, circus performers
 nerdfetch
