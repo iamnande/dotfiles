a07bca5b899a60bcb3753488c119498280324557
Nick Anderson <nick@morethq.com>
2026-03-28 23:13:38 -0700

    refactor(hm): tighten flake and module signatures
    drops unused home-manager input from flake.nix (consumer provides
    the hm context) and unused pkgs/lib args from nick.nix module.

--- flake.nix
+++ flake.nix
@@ -3,11 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, ... }: {
