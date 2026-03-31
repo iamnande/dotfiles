824841b4adb80bd4d0610ae7b1188e0a55fb6342
Nick Anderson <nick@morethq.com>
2026-03-30 22:49:54 -0700

    feat(backgrounds): migrate to home-manager module

---
+++ modules/backgrounds/default.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  home.file.".config/backgrounds" = {
+    source = ./wallpapers;
+    recursive = true;
+  };
+}
--- modules/nick.nix
+++ modules/nick.nix
@@ -1,6 +1,7 @@
 { ... }: {
 
   imports = [
+    ./backgrounds
     ./git
     ./fish
     ./helix
