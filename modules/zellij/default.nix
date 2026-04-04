cd3e29c1111427c50f10869e82cc5e4536a04bda
nick <nick@ngrok.com>
2026-04-04 01:38:04 +0000

    fix(zellij): attach to named session mhq on shell start

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -2,14 +2,18 @@
 
   programs.zellij = {
     enable = true;
-    enableFishIntegration = true;
-    attachExistingSession = true;
     settings = {
       theme = "everforest-dark";
       show_startup_tips = false;
       session_name = "mhq";
     };
   };
+
+  programs.fish.interactiveShellInit = ''
+    if not set -q ZELLIJ
+        zellij attach -c mhq
+    end
+  '';
 
   xdg.configFile."zellij/themes/everforest-dark.kdl".source = ./themes/everforest-dark.kdl;
 
